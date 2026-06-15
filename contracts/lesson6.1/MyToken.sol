8a4feeea61e0cbc35cc026d99cd75e76d029507d
liubo0208 <tyliubo0208@gmail.com>
2026-06-15 09:36:54 +0800

    openzeppelin切换5.9.6

--- contracts/lesson6.1/MyToken.sol
+++ contracts/lesson6.1/MyToken.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyToken is ERC20, Ownable {
-    constructor(uint256 initialSupply) ERC20("My Token", "MTK") Ownable() {
+    constructor(uint256 initialSupply) ERC20("My Token", "MTK") Ownable(msg.sender) {
         _mint(msg.sender, initialSupply);
     }
     
